74f335f1a2be12d1d0c026f281754cbdfd375bed
intrast <kovach.bohdan@ukr.net>
2019-10-08 12:08:20 +0300

    StorageContext

--- Storage/Models/IdentityModels.cs
+++ Storage/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Storage.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("StorageContext", throwIfV1Schema: false)
         {
         }
 
